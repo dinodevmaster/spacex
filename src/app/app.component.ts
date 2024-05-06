0543c413a2c072f06e61a376c20be33dae684f8a
Omar González <dinodev.master@gmail.com>
2024-05-06 08:43:51 -0600

    Call API SpaceX

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { NgOptimizedImage } from '@angular/common'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'tenmin';
+export class AppComponent implements OnInit{
+  data:any[] = []
+  http = inject(HttpClient);
+
+  ngOnInit(): void {
+    const url:string='https://api.spacexdata.com/v4/rockets'
+      this.http.get(url).subscribe((res: any) => {
+        this.data = res
+      })
+  }
+
 }
